1d1a3e5e88a3bace9006bf7001675ce3daaca40f
muratbaiguchukov <ambkg-muratb@yandex.ru>
2021-12-03 14:39:33 +0600

    add Hello world quartet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world het trik");
+        System.out.println("Hello world quartet");
     }
 }
