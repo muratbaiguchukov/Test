6e099d83574f3abfb1f662ef7ec282b9cbe4815b
muratbaiguchukov <ambkg-muratb@yandex.ru>
2021-12-03 18:10:00 +0600

    add encapsulation

---
+++ src/com/company/incapsulation/Main.java
@@ -0,0 +1,11 @@
+package com.company.incapsulation;
+
+import com.company.introduseOOP.Kopilka;
+
+public class Main {
+    public static void main(String[] args) {
+        Kopilka myKop = new Kopilka();
+        myKop.insertMoney(12);
+        myKop.showMoneyInKopilka();
+    }
+}
