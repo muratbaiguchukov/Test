0274c3ec9c98e812b8dfb01b94ae4d20fb4afb9e
muratbaiguchukov <ambkg-muratb@yandex.ru>
2021-12-03 17:53:56 +0600

    add Kopilka

--- src/com/company/introduseOOP/Main.java
+++ src/com/company/introduseOOP/Main.java
@@ -1,4 +1,16 @@
 package com.company.introduseOOP;
 
 public class Main {
+
+    public static void main(String[] args) {
+        // 2 вида типов данных: примитивные иссылочные
+        Kopilka myKopilka = new Kopilka();
+        myKopilka.countMoney = 22;
+        myKopilka.insertMoney(12);
+        myKopilka.insertMoney(20);
+        System.out.println(myKopilka.countMoney);
+
+
+
+    }
 }
