31f9de47e3c67c5f8a1e3597badf521bbe243b09
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-01-20 15:56:35 +0600

    add javarush

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //MyClass cl = new MyClass();
+
+        MyClass.firstMethod();
+        MyClass.cecondMethod();
+        System.out.println(Car.myStatic);
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/company/task1/Javarush.java
@@ -0,0 +1,13 @@
+package com.company.task1;
+
+public class Javarush {
+
+    /*В переменной centimeters записано количество сантиметров. В переменную meters нужно записать количество полных метров
+    в centimeters. (1 метр = 100 см). Для вычисления используй переменную centimeters и оператор деления. Для объявления и
+    инициализации meters используй одну команду.
+     */
+    int centimeters = 500;
+    meters = centimeters/100;
+    System.out.print(meters);
+
+}
