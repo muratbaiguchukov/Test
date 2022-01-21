46f2e4dbeba295b1ed8f1d55b23ab6251a59a6de
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-01-21 16:45:11 +0600

    add Variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //MyClass cl = new MyClass();
-
-        MyClass.firstMethod();
-        MyClass.cecondMethod();
-        System.out.println(Car.myStatic);
-
-
-
-
+        char[] array = {'п', 'р', 'о', 'г', 'р', 'а', 'м', 'м', 'и', 'с', 'т'};
+        System.out.println(array);
+        String s = String.valueOf(array);
+        StringBuilder s1 = new StringBuilder(s).reverse();
+        System.out.println(s1);
     }
-
 }
+
+
+
 
 
 
