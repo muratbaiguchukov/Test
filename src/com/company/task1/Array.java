e2c5439fb29f0a84681319d5768944b165f17038
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-02-10 18:52:48 +0600

    add homework-35-zad-B

--- src/com/company/task1/Array.java
+++ src/com/company/task1/Array.java
@@ -10,15 +10,27 @@ public class Array {
         myStrSong.add("Нагонял беду");
         myStrSong.add("И сказал ты мне");
         myStrSong.add("Больше не приду");
-        System.out.println(myStrSong);
-        System.out.println(myStrSong.size());
-        myStrSong.add(1, "Ветер с моря дул");
-        myStrSong.add(3, "Нагонял беду");
-        myStrSong.add(5, "И сказал ты мне");
-        myStrSong.add(7, "Больше не приду");
-        myStrSong.remove(4);
-        myStrSong.remove(4);
-        System.out.println(myStrSong);
+        for (int i = 0; i < myStrSong.size(); i++) {
+            if (myStrSong.get(i).length() > 15 ){
+                myStrSong2.add(myStrSong.get(i));
+                myStrSong2.add(myStrSong.get(i));
+            }else {
+                myStrSong2.add(myStrSong.get(i) + "-"+ myStrSong.get(i));
+            }
+        }
+        System.out.println(myStrSong2);
+        myStrSong2.remove(myStrSong2.indexOf("И сказал ты мне-И сказал ты мне"));
+        System.out.println(myStrSong2);
+
+//        System.out.println(myStrSong);
+//        System.out.println(myStrSong.size());
+//        myStrSong.add(1, "Ветер с моря дул");
+//        myStrSong.add(3, "Нагонял беду");
+//        myStrSong.add(5, "И сказал ты мне");
+//        myStrSong.add(7, "Больше не приду");
+//        myStrSong.remove(4);
+//        myStrSong.remove(4);
+//        System.out.println(myStrSong);
 
 
         }
