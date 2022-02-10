9b184fe8ebf5eaef7f9ad3573f848f485129823c
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-02-10 11:38:26 +0600

    add Variables

--- src/com/company/task2/Variables.java
+++ src/com/company/task2/Variables.java
@@ -1,17 +1,22 @@
 package com.company.task2;
 
+import java.util.*;
 import java.util.Scanner;
 
 public class Variables {
     public static void main(String[] args) {
-    System.out.print("Введите число 1:");
-    Scanner inputFigure = new Scanner(System.in);
-    int i  = inputFigure.nextInt();
-    if (i == 1) {
-    System.out.println("Вы ввели число 1");
-}
-    else {
-        System.out.println("Вы ввели число не равное 1");
-}
-}
-}
+        Integer[] integers = new Integer[5];
+        System.out.println(integers.length);
+        ArrayList<Integer> myVariebles = new ArrayList<>();
+        System.out.println(myVariebles.size());
+                                            // создать список в котором содержатся числа, которые являются квадратами.
+                                            // найти все такие числа до числа 10000
+        for (int i = 0; i < 10000; i++) {
+            myVariebles.add(i * i);
+        }
+        System.out.println(myVariebles.size());
+
+        System.out.println(myVariebles);
+
+    }
+        }
