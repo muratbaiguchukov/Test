efd26ec224f325b0732bf6b9cfa6c42e5aab5b36
muratbaiguchukov <ambkg-muratb@yandex.ru>
2022-02-21 16:51:27 +0600

    add homework-40-zad-2-hashmap

--- src/com/company/task6/Hashmap.java
+++ src/com/company/task6/Hashmap.java
@@ -20,10 +20,19 @@ public class Hashmap {
         presidents.put(PRESIDENT, "Путин");
         System.out.println(presidents);
         System.out.println(pensionerList);
-
+/* Получить список всех должностей из предыдущего задания в Set.
+Удалить какую-то должность
+Добавить.
+Проверить что стало с HashMap
+ */
         Set<String> keys = presidents.keySet();
         keys.remove(PRESIDENT);
+        System.out.println(presidents);
+            presidents.put(PRESIDENT, "Я");
+            presidents.put("ВицеPRESIDENT", "Ты");
+            presidents.put("Секретарь", "Она");
             System.out.println(presidents);
-
-    }
+            Collection<String> values = presidents.values();
+            System.out.println(presidents.values());
+        }
 }
